7dd3342c4131b6970720b1ad875e2d10f1a20f28
Praggya <119279862+praggyaverma@users.noreply.github.com>
2024-05-28 19:09:09 +0530

    Create rotateArray1.cpp

---
+++ day01/DSA/rotateArray1.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotateK(vector<int>& arr, int k){
+	k = k % arr.size();
+	reverse(arr.begin(), arr.end());
+	
+	reverse(arr.begin(), arr.begin() + k);
+	
+	reverse(arr.begin() + k, arr.end());
+}
+
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> vec;
+	
+	for(int i = 0; i < n; i++){
+		int x;
+		cin >> x;
+		vec.push_back(x);
+	}
+	
+	int k;
+	cin >> k;
+	
+	rotateK(vec, k);
+	
+	for(int i = 0; i < n; i++){
+		cout << vec[i] << " ";
+	}	
+	
+	return 0;
+}
